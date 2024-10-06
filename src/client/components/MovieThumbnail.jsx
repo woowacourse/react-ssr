3fd107aba356e98e947622873cf673fd3b6d0355
ss0526100 <ss0526100@gmail.com>
2024-10-06 22:32:26 +0900

    feat:영화 썸네일 구현

---
+++ src/client/components/MovieThumbnail.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TMDB_THUMBNAIL_URL } from '../constants';
+import startEmptyPng from '../../../public/images/star_empty.png';
+
+export default function MovieThumbnail({ movie }) {
+  return (
+    <li>
+      <div className='item'>
+        <img
+          className='thumbnail'
+          src={`${TMDB_THUMBNAIL_URL}${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div className='item-desc'>
+          <p className='rate'>
+            <img src={startEmptyPng} className='star' />
+            <span>{movie.vote_average.toFixed(1)}</span>
+          </p>
+          <strong>{movie.title}</strong>
+        </div>
+      </div>
+    </li>
+  );
+}
