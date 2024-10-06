65db6e25e7d038eb7e00468bac8184ab3d165562
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-10-06 09:15:09 +0900

    feat: movies 컴포넌트 구현

--- src/client/components/Movies.jsx
+++ src/client/components/Movies.jsx
@@ -1,9 +1,36 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { TMDB_THUMBNAIL_URL } from "../../constants";
+
+const round = (value, decimals = 0) => {
+  const factor = 10 ** decimals;
+  return Math.round(value * factor) / factor;
+};
+
+function MovieItem({ rate, title, thumbnailUrl, onClick }) {
+  const thumbnailFullUrl = TMDB_THUMBNAIL_URL + "/" + thumbnailUrl;
+
+  return (
+    <div className="item" onClick={onClick}>
+      <img className="thumbnail" src={thumbnailFullUrl} alt={title} />
+      <div className="item-desc">
+        <p className="rate">
+          <img src={"/assets/images/star_empty.png"} className="star" />
+          <span>{round(rate, 1)}</span>
+        </p>
+        <strong>{title}</strong>
+      </div>
+    </div>
+  );
+}
 
 export default function Movies({ initData }) {
-  console.log(initData);
-  useEffect(() => {
-    console.log("hydrate");
-  }, []);
-  return <div>movies</div>;
+  return (
+    <>
+      {initData?.movies.map(({ id, title, vote_average, poster_path }) => (
+        <li key={id}>
+          <MovieItem rate={vote_average} title={title} thumbnailUrl={poster_path} />
+        </li>
+      ))}
+    </>
+  );
 }
