038bb60528f53c652a92b85984de29605c074c09
jinsil <jinsil378900@gmail.com>
2024-10-06 23:58:23 +0900

    feat: api 연결 상수, 함수 추가

---
+++ src/server/api.js
@@ -0,0 +1,8 @@
+import { TMDB_MOVIE_NOW_PLAYING_LISTS } from "./constant.js";
+import { FETCH_OPTIONS } from "./constant.js";
+
+export const fetchNowPlayingMovies = async () => {
+  const response = await fetch(TMDB_MOVIE_NOW_PLAYING_LISTS, FETCH_OPTIONS);
+
+  return await response.json();
+};
