12dc22a10ea8e678f4323df5a15c1f813d4db1a1
김진호 <rlawlsgh1227@gmail.com>
2024-10-06 21:19:20 +0900

    feat: 영화 정보 받아서 컴포넌트에 내용 채움

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,28 +6,53 @@ import { fileURLToPath } from "url";
 import React from "react";
 import { renderToString } from "react-dom/server";
 import App from "../../client/App";
+import { FETCH_OPTIONS } from "../../constants/fetch";
+import { TMDB_MOVIE_LISTS } from "../../constants/tmdb";
+import TopRatedMovieView from "../../client/components/topRatedMovie/TopRatedMovieView";
+import MovieItem from "../../client/components/MovieItem";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../../views", "index.html");
-  const renderedApp = renderToString(<App />);
+const loadMovies = async (url) => {
+  const response = await fetch(url, FETCH_OPTIONS);
+  const data = await response.json();
 
+  return data.results;
+};
+
+const getTopRateMovie = (movies) => {
+  const topRateMovie = movies.reduce((highest, movie) => {
+    return movie.vote_average > highest.vote_average ? movie : highest;
+  }, movies[0]);
+
+  return topRateMovie;
+};
+
+router.get("/", async (_, res) => {
+  const templatePath = path.join(__dirname, "../../../views", "index.html");
   const template = fs.readFileSync(templatePath, "utf-8");
-  // const initData = template.replace(
-  //   "<!--${INIT_DATA_AREA}-->",
-  //   /*html*/ `
-  //   <script>
-  //     window.__INITIAL_DATA__ = {
-  //       movies: ${JSON.stringify(popularMovies)}
-  //     }
-  //   </script>
-  // `
-  // );
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", renderedApp);
+
+  const popularMovies = await loadMovies(TMDB_MOVIE_LISTS.popular);
+  const renderedApp = renderToString(<App movies={popularMovies} />);
+
+  const topRatedMovie = getTopRateMovie(popularMovies);
+
+  const topRatedMovieHTML = renderToString(<TopRatedMovieView movie={topRatedMovie} />);
+
+  const movieListHTML = renderToString(
+    <>
+      {popularMovies.map((movie) => (
+        <MovieItem key={movie.id} movie={movie} />
+      ))}
+    </>
+  );
+
+  const renderedHTML = template
+    .replace("<!--${TOP_RATED_MOVIE_PLACEHOLDER}-->", topRatedMovieHTML)
+    .replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", movieListHTML);
 
   res.send(renderedHTML);
 });
