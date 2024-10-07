66e3f39b95d4e0161b829c0d22fdedbca5256f41
badahertz52 <badahertz52@gmail.com>
2024-10-08 00:08:10 +0900

    feat: SSR 구현
     - 영화 데이터 불러와 클라이언트에 넘기고 렌더링

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,29 +5,34 @@ import { fileURLToPath } from "url";
 
 import React from "react";
 import { renderToString } from "react-dom/server";
-import App from "../../client/App";
+import { fetchPopularMovieList } from "../apis/handler";
+import App from "@src/client/App";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
+router.get("/", async (_, res) => {
+  const popularMovieList = await fetchPopularMovieList();
+
   const templatePath = path.join(__dirname, "../../../views", "index.html");
-  const renderedApp = renderToString(<App />);
+  if (!popularMovieList[0]) return templatePath;
+
+  const renderedApp = renderToString(<App movieList={popularMovieList} />);
 
   const template = fs.readFileSync(templatePath, "utf-8");
-  // const initData = template.replace(
-  //   "<!--${INIT_DATA_AREA}-->",
-  //   /*html*/ `
-  //   <script>
-  //     window.__INITIAL_DATA__ = {
-  //       movies: ${JSON.stringify(popularMovies)}
-  //     }
-  //   </script>
-  // `
-  // );
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", renderedApp);
+  const initData = template.replace(
+    "<!--${INIT_DATA_AREA}-->",
+    /*html*/ `
+    <script>
+      window.__INITIAL_DATA__ = {
+        movies: ${JSON.stringify(popularMovieList)}
+      }
+    </script>
+  `
+  );
+  const renderedHTML = initData.replace("<!--${APP_AREA}-->", renderedApp);
 
   res.send(renderedHTML);
 });
