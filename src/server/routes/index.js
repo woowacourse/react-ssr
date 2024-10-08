8c9afb03c9eccd04f1c01f3233df2406c8aa6b75
Cron <hojoong.choi@woowahan.com>
2024-10-08 23:13:55 +0900

    fix: 뼈대 코드 수정 및 샘플 코드 제공

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,22 +1,14 @@
-import { Router } from "express";
+import App from "../../client/App";
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 
 import React from "react";
+import { Router } from "express";
 import { renderToString } from "react-dom/server";
-import App from "../../client/App";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../../views", "index.html");
-  const renderedApp = renderToString(<App />);
-
-  const template = fs.readFileSync(templatePath, "utf-8");
+router.use("/", async (_, res) => {
   // const initData = template.replace(
   //   "<!--${INIT_DATA_AREA}-->",
   //   /*html*/ `
@@ -27,9 +19,48 @@ router.get("/", (_, res) => {
   //   </script>
   // `
   // );
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", renderedApp);
 
-  res.send(renderedHTML);
+  const movies = [
+    {
+      adult: false,
+      backdrop_path: "/lgkPzcOSnTvjeMnuFzozRO5HHw1.jpg",
+      genre_ids: [16, 10751, 35, 28],
+      id: 519182,
+      original_language: "en",
+      original_title: "Despicable Me 4",
+      overview:
+        "슈트-업 하고 악당 전담 처리반 AVL이 된 에이전트 미니언즈와 미니언즈 만큼 귀여운 그루 주니어가 태어나면서 더욱 완벽해진 그루 패밀리. 이들 앞에 과거 그루의 고등학교 동창이자 그에게 체포당했던 빌런 맥심이 등장하고, 오직 그루를 향한 복수심에 불타올라 탈옥까지 감행한 맥심은 그루 패밀리의 뒤를 바짝 추격하며 위협을 가하기 시작하는데... 과연 에이전트 미니언즈와 그루 패밀리는 맥심을 막아낼 수 있을까?",
+      popularity: 1854.122,
+      poster_path: "/5hl1PEpAvZ8Ok37kB7woIssHi3X.jpg",
+      release_date: "2024-06-20",
+      title: "슈퍼배드 4",
+      video: false,
+      vote_average: 7.139,
+      vote_count: 1905,
+    },
+    {
+      adult: false,
+      backdrop_path: "/yDHYTfA3R0jFYba16jBB1ef8oIt.jpg",
+      genre_ids: [28, 35, 878],
+      id: 533535,
+      original_language: "en",
+      original_title: "Deadpool & Wolverine",
+      overview:
+        "히어로 생활에서 은퇴한 후, 평범한 중고차 딜러로 살아가던 ‘데드풀’이 예상치 못한 거대한 위기를 맞아 모든 면에서 상극인 ‘울버린’을 찾아가게 되며 펼쳐지는 도파민 폭발 액션 블록버스터.",
+      popularity: 4025.483,
+      poster_path: "/4Zb4Z2HjX1t5zr1qYOTdVoisJKp.jpg",
+      release_date: "2024-07-24",
+      title: "데드풀과 울버린",
+      video: false,
+      vote_average: 7.739,
+      vote_count: 4103,
+    },
+  ];
+  const renderedApp = renderToString(<App movies={movies} />);
+  const templatePath = path.resolve(__dirname, "index.html");
+  const template = fs.readFileSync(templatePath, "utf8");
+
+  res.send(template.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`));
 });
 
 export default router;
