473093077d6c1204a0d427ce580982259557540a
pakxe <pigkill40@naver.com>
2024-10-09 01:23:45 +0900

    feat: 서버에서 데이터를 불러와 App을 만들어 서빙하도록 함

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,33 +1,58 @@
-import { Router } from "express";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import { Router } from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import React from "react";
-import { renderToString } from "react-dom/server";
-import App from "../../client/App";
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from '../../client/App';
+import { FETCH_OPTIONS, TMDB_MOVIE_LISTS } from '../constants.js';
+import MovieList from '../../client/components/MovieList';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../../views", "index.html");
-  const renderedApp = renderToString(<App />);
+const parseMovieItem = (movieItemData) => {
+  return {
+    id: movieItemData.id,
+    title: movieItemData.title,
+    rate: movieItemData.vote_average,
+    background: movieItemData.backdrop_path,
+  };
+};
 
-  const template = fs.readFileSync(templatePath, "utf-8");
-  // const initData = template.replace(
-  //   "<!--${INIT_DATA_AREA}-->",
-  //   /*html*/ `
-  //   <script>
-  //     window.__INITIAL_DATA__ = {
-  //       movies: ${JSON.stringify(popularMovies)}
-  //     }
-  //   </script>
-  // `
-  // );
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", renderedApp);
+const parseMovieList = (movieList) => {
+  return movieList.results.map((item) => parseMovieItem(item));
+};
+
+const fetchNowPlayingMovies = async () => {
+  const response = await fetch(TMDB_MOVIE_LISTS.NOW_PLAYING, FETCH_OPTIONS);
+  const data = await response.json();
+
+  return parseMovieList(data);
+};
+
+router.get('/', async (_, res) => {
+  const templatePath = path.join(__dirname, '../../../views', 'index.html');
+  const movieList = await fetchNowPlayingMovies();
+  const renderedApp = renderToString(<App movieList={movieList} />);
+
+  const template = fs.readFileSync(templatePath, 'utf-8');
+
+  const initData = template.replace(
+    '<!--${INIT_DATA_AREA}-->',
+    /*html*/ `
+  <script>
+    window.__INITIAL_DATA__ = {
+      movieList: ${JSON.stringify(movieList)}
+    }
+  </script>
+`,
+  );
+
+  const renderedHTML = template.replace('<!--${APP_AREA}-->', renderedApp);
 
   res.send(renderedHTML);
 });
