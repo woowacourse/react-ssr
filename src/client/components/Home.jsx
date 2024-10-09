e265f07f1970ac2ba9b69c48e809dc6e6f62ee66
Lee sang Yeop <mma7710@naver.com>
2024-10-09 14:50:47 +0900

    홈 컴포넌트 추가

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,12 +1,31 @@
-import React from "react";
-import starEmptyImage from "@images/star_empty.png";
+import { useEffect } from 'react';
+import MovieItem from './base/MovieItem';
+import useMovies from '../hooks/useMovies';
+import React from 'react';
 
-function Home() {
-  return (
-    <div>
-      <img src={starEmptyImage} />
-    </div>
-  );
+function Home({ movies }) {
+	const { popularMovies, loadMovies } = useMovies();
+
+	useEffect(() => {
+		loadMovies();
+	}, []);
+
+	return (
+		<div className="container">
+			<main>
+				<section>
+					<h2>지금 인기 있는 영화</h2>
+					<ul className="thumbnail-list">
+						{movies.map(({ id, title, vote_average, poster_path }) => (
+							<li key={id}>
+								<MovieItem rate={vote_average} title={title} thumbnailUrl={poster_path} />
+							</li>
+						))}
+					</ul>
+				</section>
+			</main>
+		</div>
+	);
 }
 
 export default Home;
