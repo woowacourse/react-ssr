82135392c0eb81d39328038cd6024b3f8e52076d
bmj0053 <bmj05378@gmail.com>
2024-10-09 18:46:12 +0900

    feat: 영화 데이터 호출 구현

---
+++ src/api/constants.js
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://api.themoviedb.org/3/movie';
+export const TMDB_MOVIE_LISTS = {
+  NOW_PLAYING: BASE_URL + '/now_playing?language=ko-KR&page=1',
+};
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,33 +1,32 @@
-import { Router } from "express";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import { Router } from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import React from "react";
-import { renderToString } from "react-dom/server";
-import App from "../../client/App";
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from '../../client/App.jsx';
+import { fetchMovieList } from '../../api/fetchMovieList.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../../views", "index.html");
-  const renderedApp = renderToString(<App />);
+router.get('/', async (_, res) => {
+  const templatePath = path.join(__dirname, '../../../views', 'index.html');
+  const movieListData = await fetchMovieList(); // API 호출
+  const renderedApp = renderToString(<App movieList={movieListData ?? []} />);
 
-  const template = fs.readFileSync(templatePath, "utf-8");
-  // const initData = template.replace(
-  //   "<!--${INIT_DATA_AREA}-->",
-  //   /*html*/ `
-  //   <script>
-  //     window.__INITIAL_DATA__ = {
-  //       movies: ${JSON.stringify(popularMovies)}
-  //     }
-  //   </script>
-  // `
-  // );
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", renderedApp);
+  const template = fs.readFileSync(templatePath, 'utf-8');
+  const initData = /*html*/ `
+    <script>
+      window.__INITIAL_DATA__ = ${JSON.stringify(movieListData)};
+    </script>
+  `;
+  const renderedHTML = template
+    .replace('<!--${INIT_DATA_AREA}-->', initData)
+    .replace('<!--${MOVIE_ITEMS_PLACEHOLDER}-->', renderedApp);
 
   res.send(renderedHTML);
 });
