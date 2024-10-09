eece6327a90f971d4ef8a4b8169d6fba9121c4a8
Hyelim Choi <HyelimChoi01@gmail.com>
2024-10-09 15:11:07 +0900

    chore: 크론의 추가 설정

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <img src={starEmptyImage} />
+    </div>
+  );
 }
 
 export default Home;
