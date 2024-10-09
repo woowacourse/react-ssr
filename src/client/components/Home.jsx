38b37bd542d15aefdca41b665a60a00b8a4e425b
Kim Da Eun <midekuna@gmail.com>
2024-10-09 22:22:00 +0900

    feat: 인기 영화 페이지 ssr

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import starEmptyImage from '@images/star_empty.png';
+import Header from './Header';
+import MovieItem from './MovieItem';
 
-function Home() {
+function Home({ movies }) {
   return (
     <div>
-      <img src={starEmptyImage} />
+      <Header bannerMovie={movies[0]} />
+      <div className="container">
+        <main>
+          <section>
+            <h2>지금 인기 있는 영화</h2>
+            <ul className="thumbnail-list">
+              {movies.map((movie) => (
+                <li key={movie.id}>
+                  <MovieItem movie={movie} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
