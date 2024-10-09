188fdc979625663270786898cdb4cfbbf4626011
useon <roqkftjs@gmail.com>
2024-10-09 17:28:44 +0900

    feat: Header 컴포넌트에 가장 인기 있는 영화 정보 렌더링

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Header from './components/Header.jsx';
+
 function App({ movies }) {
-  return <div id='wrap'></div>;
+  const bestMovieItem = movies[0];
+
+  return (
+    <div id='wrap'>
+      <Header bestMovieItem={bestMovieItem} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/client/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TMDB_BANNER_URL } from '../../server/constants/constant';
+
+const Header = ({ bestMovieItem }) => {
+  const { backdrop_path, vote_average, title } = bestMovieItem;
+  const bannerUrl = TMDB_BANNER_URL + backdrop_path;
+
+  return (
+    <header>
+      <div
+        className='background-container'
+        style={{ backgroundImage: `url('${bannerUrl}')` }}
+      >
+        <div className='overlay' aria-hidden='true'></div>
+        <div className='top-rated-container'>
+          <h1 className='logo'>
+            <img src='../assets/images/logo.png' alt='MovieList' />
+          </h1>
+          <div className='top-rated-movie'>
+            <div className='rate'>
+              <img src='../assets/images/star_empty.png' className='star' />
+              <span className='rate-value'>{vote_average}</span>
+            </div>
+            <div className='title'>{title}</div>
+            <button className='primary detail'>자세히 보기</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
