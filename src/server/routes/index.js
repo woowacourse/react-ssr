851caf15199dc8f00bca6fa36bad01a5eb774d45
bmj0053 <bmj05378@gmail.com>
2024-10-09 18:51:25 +0900

    feat: header의 bestMovieItem 설정

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -7,6 +7,7 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import App from '../../client/App.jsx';
 import { fetchMovieList } from '../../api/fetchMovieList.js';
+import { TMDB_BANNER_URL } from '../../api/constants.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,6 +18,8 @@ router.get('/', async (_, res) => {
   const templatePath = path.join(__dirname, '../../../views', 'index.html');
   const movieListData = await fetchMovieList(); // API 호출
   const renderedApp = renderToString(<App movieList={movieListData ?? []} />);
+  const bestMovieItem =
+    movieListData && movieListData.length > 0 ? movieListData[0] : {};
 
   const template = fs.readFileSync(templatePath, 'utf-8');
   const initData = /*html*/ `
@@ -26,7 +29,14 @@ router.get('/', async (_, res) => {
   `;
   const renderedHTML = template
     .replace('<!--${INIT_DATA_AREA}-->', initData)
-    .replace('<!--${MOVIE_ITEMS_PLACEHOLDER}-->', renderedApp);
+    .replace('<!--${MOVIE_ITEMS_PLACEHOLDER}-->', renderedApp)
+    .replace(
+      '${background-container}',
+      TMDB_BANNER_URL + bestMovieItem.backdrop_path
+    )
+    .replace('${bestMovie.rate}', bestMovieItem.vote_average)
+    .replace('${bestMovie.rate}', bestMovieItem.vote_average)
+    .replace('${bestMovie.title}', bestMovieItem.title);
 
   res.send(renderedHTML);
 });
