eb5689c11ea2d5fb91ad792a8788b25f216acdb7
손진영 <sm099@naver.com>
2024-10-09 15:06:28 +0900

    refactor(render): ssr 렌더링 로직 개선
    1. createHTMLTemplate 내 html url path 변경
    
    2. 초기 데이터 스크립트 생성 로직 인라인으로 변경
    
    3. html 템플릿 내 루트 요소 교체 로직 수정

--- src/server/utils/render.js
+++ src/server/utils/render.js
@@ -1,33 +1,18 @@
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 
 import { fetchNowPlayingMovies } from '../apis/movies.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 import App from '../../client/App';
 
 export function createHTMLTemplate() {
-  const templatePath = path.join(__dirname, '../../../views', 'index.html');
+  const templatePath = path.resolve(__dirname, 'index.html');
   const template = fs.readFileSync(templatePath, 'utf-8');
 
   return template;
-}
-
-function createInitialDataScript(movies) {
-  return `
-      <script>
-        window.__INITIAL_DATA__ = {
-          movies: ${JSON.stringify(movies)}
-        }
-      </script>
-      <script type="module" src="/bundle"></script>
-    `;
 }
 
 export async function renderHTML() {
@@ -37,7 +22,14 @@ export async function renderHTML() {
 
   const renderedApp = renderToString(<App movies={nowPlayingMovies} />);
 
-  return template
-    .replace('<!--${MOVIE_ITEMS_PLACEHOLDER}-->', renderedApp)
-    .replace('<!--${INIT_DATA_AREA}-->', createInitialDataScript(nowPlayingMovies));
+  return template.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`).replace(
+    '<!--${INIT_DATA_AREA}-->',
+    `
+      <script>
+        window.__INITIAL_DATA__ = {
+          movies: ${JSON.stringify(nowPlayingMovies)}
+        }
+      </script>
+    `
+  );
 }
