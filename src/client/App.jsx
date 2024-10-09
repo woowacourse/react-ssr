c48e21a81e1f74e7562c2c8b5c3be32768390c7d
Kim Da Eun <midekuna@gmail.com>
2024-10-09 23:48:29 +0900

    feat: 푸터 추가

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App({ movies }) {
-  return <Home movies={movies} />;
+  return (
+    <>
+      <Home movies={movies} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
