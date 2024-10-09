7d5046252290b1598d78e42e8e52f24a1734eb04
soosoo22 <lllllh00@naver.com>
2024-10-09 22:59:59 +0900

    feat: ssr 구현

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-import Home from "./components/Home";
 
-function App() {
+import Header from "./components/Header.jsx";
+import MovieList from "./components/MovieList.jsx";
+
+const App = ({ movies }) => {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Header movie={movies[0]} />
+      <div className="container">
+        <main>
+          <section>
+            <h2>지금 인기 있는 영화</h2>
+            <ul className="thumbnail-list">
+              <MovieList movies={movies} />
+            </ul>
+          </section>
+        </main>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
