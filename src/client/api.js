f57033a224c80dc2f4f761212dfc509058ab54db
Lee sang Yeop <mma7710@naver.com>
2024-10-09 21:42:00 +0900

    TMDB 요청 시 영화 데이터만 반환하도록 변경

--- src/client/api.js
+++ src/client/api.js
@@ -1,8 +1,8 @@
-import { FETCH_OPTIONS, TMDB_MOVIE_LISTS } from './Constant';
+import { FETCH_OPTIONS, TMDB_MOVIE_LISTS } from "./Constant";
 
 export const fetchPopularMovies = async () => {
-	const response = await fetch(TMDB_MOVIE_LISTS.popular, FETCH_OPTIONS);
-	const data = await response.json();
+  const response = await fetch(TMDB_MOVIE_LISTS.popular, FETCH_OPTIONS);
+  const data = await response.json();
 
-	return data;
+  return data.results;
 };
