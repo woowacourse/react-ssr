2df233d1e543a56521a14f2bf0926163d57313c8
hwinkr <dnddl8280@naver.com>
2024-10-09 18:03:39 +0900

    feat: App 컴포넌트가 전역객체의 초기 데이터를 받을 수 있도록 구현

--- src/client/main.js
+++ src/client/main.js
@@ -2,4 +2,6 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
 
-hydrateRoot(document.getElementById("root"), <App />);
+const { movies } = window.__INITIAL_DATA__;
+
+hydrateRoot(document.getElementById("root"), <App movies={movies} />);
