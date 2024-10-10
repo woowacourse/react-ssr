a23482124b96113d6cc5d7029077998497880762
GimGayeon <kky1373@naver.com>
2024-10-10 10:52:25 +0900

    refactor: replace 로직 분리

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,38 +1,42 @@
-import App from "../../client/App";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { Router } from "express";
 import fs from "fs";
 import path from "path";
-
-import React from "react";
-import { Router } from "express";
-import { renderToString } from "react-dom/server";
+import App from "../../client/App";
 import { fetchNowPlayingMovieItems } from "../../apis/fetchMovies";
 
 const router = Router();
 
-router.use("/", async (_, res) => {
-  const movies = (await fetchNowPlayingMovieItems()) || [];
-  const renderedApp = renderToString(<App movies={movies} />);
-
-  const templatePath = path.resolve(__dirname, "index.html");
-  const template = fs.readFileSync(templatePath, "utf8");
-
-  const initData = template.replace(
-    "<!--${INIT_DATA_AREA}-->",
-    /*html*/ `
+function getInitialDataScript(movies) {
+  return `
     <script>
       window.__INITIAL_DATA__ = {
-        movies:${JSON.stringify(movies)},
-      }
+        movies: ${JSON.stringify(movies)}
+      };
     </script>
-  `
-  );
+  `;
+}
 
-  res.send(
-    initData.replace(
-      '<div id="root"></div>',
-      `<div id="root">${renderedApp}</div>`
-    )
-  );
+router.use("/", async (_, res) => {
+  try {
+    const movies = (await fetchNowPlayingMovieItems()) || [];
+    const renderedApp = renderToString(<App movies={movies} />);
+
+    const templatePath = path.resolve(__dirname, "index.html");
+    const template = fs.readFileSync(templatePath, "utf8");
+
+    const initialDataScript = getInitialDataScript(movies);
+
+    const renderedHTML = template
+      .replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`)
+      .replace("<!--${INIT_DATA_AREA}-->", initialDataScript(movies));
+
+    res.send(renderedHTML);
+  } catch (error) {
+    console.error("렌더링 과정에서 오류 발생", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 export default router;
