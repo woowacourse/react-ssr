3b3c48d917e1dbdcb5a24946b6c7870744b18fc8
Seongjin Hong <hongseongjin.to@gmail.com>
2024-10-10 14:42:46 +0900

    refactor: 전역 객체 window 및 초기데이터 유무에 따른 분기 처리 추가

--- src/client/main.js
+++ src/client/main.js
@@ -4,10 +4,13 @@ import { hydrateRoot } from "react-dom/client";
 import Header from "./components/Header";
 import MovieList from "./components/MovieList";
 
-const movies = window.__INITIAL_DATA__.movies;
+const INITIAL_DATA = window ? window.__INITIAL_DATA__ || {} : {};
 
-hydrateRoot(document.getElementById("header"), <Header movie={movies[0]} />);
+hydrateRoot(
+  document.getElementById("header"),
+  <Header movie={INITIAL_DATA.movies[0]} />
+);
 hydrateRoot(
   document.getElementById("movie-list"),
-  <MovieList movies={movies} />
+  <MovieList movies={INITIAL_DATA.movies} />
 );
