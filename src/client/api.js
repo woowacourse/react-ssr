25e31589747a8f000276b22429770b5940ff2d3f
Lee sang Yeop <mma7710@naver.com>
2024-10-11 11:46:24 +0900

    [서버 사이드 렌더링 - 1단계] 다르(이상엽) 미션 제출합니다. (#22)
    * fix: 뼈대 코드 수정 및 샘플 코드 제공
    
    * 이전 미션에 사용된 코드 붙여넣기 + 불필요한 코드 제거
    
    * TMDB 서버에 요청하는 API 로직은 모듈화
    
    * Header, Footer 제작 후 App 컴포넌트에 추가
    
    * html 파일에 서버의 init_data 추가
    
    * 홈 컴포넌트 추가
    
    * 서버에서 완성된 HTML 반환하도록 변경
    
    * 불필요한 코드 제거
    
    * pretterrc 파일 추가
    
    * TMDB 요청 시 영화 데이터만 반환하도록 변경
    
    * 클라이언트는 HTML 파일의 __INITIAL_DATA__를 활용하여 렌더링하도록 변경
    
    * 불필요한 콘솔로그 제거
    
    * 사용하지 않는 onClick 함수 제거
    
    ---------
    
    Co-authored-by: Cron <hojoong.choi@woowahan.com>

---
+++ src/client/api.js
@@ -0,0 +1,8 @@
+import { FETCH_OPTIONS, TMDB_MOVIE_LISTS } from "./Constant";
+
+export const fetchPopularMovies = async () => {
+  const response = await fetch(TMDB_MOVIE_LISTS.popular, FETCH_OPTIONS);
+  const data = await response.json();
+
+  return data.results;
+};
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,35 +1,44 @@
-import { Router } from "express";
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 
 import React from "react";
+import { Router } from "express";
 import { renderToString } from "react-dom/server";
-import App from "../../client/App";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { fetchPopularMovies } from "../../client/api";
+import MoviePage from "../../client/components/MoviePage";
+import Footer from "../../client/components/Footer";
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../../views", "index.html");
-  const renderedApp = renderToString(<App />);
+router.use("/", async (_, res) => {
+  const popularMovies = await fetchPopularMovies();
 
-  const template = fs.readFileSync(templatePath, "utf-8");
-  // const initData = template.replace(
-  //   "<!--${INIT_DATA_AREA}-->",
-  //   /*html*/ `
-  //   <script>
-  //     window.__INITIAL_DATA__ = {
-  //       movies: ${JSON.stringify(popularMovies)}
-  //     }
-  //   </script>
-  // `
-  // );
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", renderedApp);
+  const templatePath = path.resolve(__dirname, "index.html");
+  const template = fs.readFileSync(templatePath, "utf8");
+  const initData = template.replace(
+    "<!--${INIT_DATA_AREA}-->",
+    /*html*/ `
+    <script>
+      window.__INITIAL_DATA__ = {
+        movies: ${JSON.stringify(popularMovies)}
+      }
+    </script>
+  `
+  );
 
-  res.send(renderedHTML);
+  const renderedApp = renderToString(
+    <div>
+      <MoviePage movies={popularMovies} />
+      <Footer />
+    </div>
+  );
+
+  res.send(
+    initData.replace(
+      '<div id="root"></div>',
+      `<div id="root">${renderedApp}</div>`
+    )
+  );
 });
 
 export default router;
