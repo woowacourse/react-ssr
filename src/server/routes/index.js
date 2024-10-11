6eede017c2664a08640b46015a572873a5384ab2
이태훈 <rhymint@gmail.com>
2024-10-11 17:47:52 +0900

    fix: 초기 데이터를 넣어주도록 변경

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -16,7 +16,18 @@ router.use('/', async (_, res) => {
   const templatePath = path.resolve(__dirname, 'index.html');
   const template = fs.readFileSync(templatePath, 'utf8');
 
-  res.send(template.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`));
+  const initData = template.replace(
+    '<!--${INIT_DATA_AREA}-->',
+    /*html*/ `
+    <script>
+      window.__INITIAL_DATA__ = {
+        movies: ${JSON.stringify(movies)}
+      }
+    </script>
+  `,
+  );
+
+  res.send(initData.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`));
 });
 
 export default router;
