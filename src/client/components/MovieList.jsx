d0726e6d7d5735f36f542e94f8409a09058d7346
hanyoung <phanyoung@naver.com>
2024-10-12 22:44:55 +0900

    refactor: MovieItem을 별도의 컴포넌트로 분리

--- src/client/components/MovieList.jsx
+++ src/client/components/MovieList.jsx
@@ -1,33 +1,19 @@
 import React from "react";
-import { TMDB_THUMBNAIL_URL } from "../constants";
+import MovieItem from "./MovieItem";
+import { Link } from "react-router-dom";
 
 function MovieList({ movies }) {
   return (
     <ul className="thumbnail-list">
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <MovieItem key={movie.id} movie={movie} />
-        </li>
+        <Link key={movie.id} to={`/detail/${movie.id}`}>
+          <li>
+            <MovieItem key={movie.id} movie={movie} />
+          </li>
+        </Link>
       ))}
     </ul>
   );
 }
 
 export default MovieList;
-
-function MovieItem({ movie }) {
-  const { poster_path, vote_average, title } = movie;
-
-  return (
-    <div className="item">
-      <img className="thumbnail" src={`${TMDB_THUMBNAIL_URL}/${poster_path}`} alt="" />
-      <div className="item-desc">
-        <p className="rate">
-          <img src="/assets/images/star_empty.png" className="star" />
-          <span>{vote_average.toFixed(1)}</span>
-        </p>
-        <strong>{title}</strong>
-      </div>
-    </div>
-  );
-}
