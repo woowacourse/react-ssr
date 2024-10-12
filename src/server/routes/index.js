e129bdaead485baa5f0aec29272fe65b38bde07d
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-10-13 00:32:42 +0900

    [서버 사이드 렌더링 - 1단계] 제이드(전세영) 미션 제출합니다. (#32)
    * fix: 뼈대 코드 수정 및 샘플 코드 제공
    
    * feat: CSR 기본 구현 및 SSR 연동
    
    * feat: 초기 movies json을 script로 전달
    
    * feat: ssr에서 api 호출 통해 영화정보 전달구현
    
    * chore: index.html INIT_DATA_AREA body로 이동
    
    ---------
    
    Co-authored-by: Cron <hojoong.choi@woowahan.com>

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,35 +1,30 @@
-import { Router } from "express";
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
-
-import React from "react";
-import { renderToString } from "react-dom/server";
 import App from "../../client/App";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { Router } from "express";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { loadNowPlaying } from "../api/loadMovies";
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../../views", "index.html");
-  const renderedApp = renderToString(<App />);
-
-  const template = fs.readFileSync(templatePath, "utf-8");
-  // const initData = template.replace(
-  //   "<!--${INIT_DATA_AREA}-->",
-  //   /*html*/ `
-  //   <script>
-  //     window.__INITIAL_DATA__ = {
-  //       movies: ${JSON.stringify(popularMovies)}
-  //     }
-  //   </script>
-  // `
-  // );
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", renderedApp);
-
-  res.send(renderedHTML);
+router.use("/", async (_, res) => {
+  const movies = await loadNowPlaying();
+  const renderedApp = renderToString(<App movies={movies} />);
+  const templatePath = path.resolve(__dirname, "index.html");
+  const template = fs.readFileSync(templatePath, "utf8");
+  const initData = template.replace(
+    "<!--${INIT_DATA_AREA}-->",
+    /*html*/ `
+    <script>
+      window.__INITIAL_DATA__ = {
+        movies: ${JSON.stringify(movies)}
+      }
+    </script>
+  `
+  );
+  res.send(initData.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`));
 });
 
 export default router;
