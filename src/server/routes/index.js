8015eee8046db5153a766af42a05758126e834af
00kang <kdb070970@gmail.com>
2024-10-12 17:03:06 +0900

    feat: 뼈대코드에 맞게 수정

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,15 +1,11 @@
 import App from "../../client/App";
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 
 import React from "react";
 import { Router } from "express";
 import { renderToString } from "react-dom/server";
 import { fetchMovies } from "../apis/movies";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const router = Router();
 
@@ -17,9 +13,16 @@ router.use("/", async (_, res) => {
   const movies = await fetchMovies();
 
   const renderedApp = renderToString(<App movies={movies} />);
-  const templatePath = path.resolve(__dirname, "../../../views/index.html");
-  const template = fs.readFileSync(templatePath, "utf8");
-  res.send(template.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`));
+  const templatePath = path.resolve(__dirname, "index.html");
+  let template = fs.readFileSync(templatePath, "utf8");
+
+  template = template.replace(
+    '<div id="root"></div>',
+    `<div id="root">${renderedApp}</div>
+     <script>window.__INITIAL_DATA__ = ${JSON.stringify({ movies })}</script>`
+  );
+
+  res.send(template);
 });
 
 export default router;
