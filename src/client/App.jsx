43c6797d1bdc03d534db001d9bf54c0e1f79cc1e
bmj0053 <bmj05378@gmail.com>
2024-10-12 17:07:49 +0900

    refactor: movieList props 기본값 로직 제거

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MovieList from './components/MovieList.jsx';
 
-function App({ movieList = [] }) {
+function App({ movieList }) {
   return <MovieList movieList={movieList} />;
 }
 
