3d0d4abfcece97682fa5ec81ba515da9dfb7e75d
pakxe <pigkill40@naver.com>
2024-10-13 14:38:19 +0900

    chore: 불필요하게 들어간 텍스트 제거

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -3,13 +3,13 @@ import { round } from '../utils';
 import React from 'react'; // 왜 react import 없으면 안되는거지?
 
 function Header({ movie }) {
-  const { id, title, rate, background } = movie;
+  const { title, rate, background } = movie;
 
   const bannerUrl = TMDB_BANNER_URL + background;
 
   return (
     <header>
-      <div className='background-container' style={{ backgroundImage: `url('${bannerUrl}')` }}>
+      <div className='background-container' style={{ backgroundImage: `url("${bannerUrl}")` }}>
         <div className='overlay' aria-hidden='true'></div>
         <div className='top-rated-container'>
           <h1 className='logo'>
@@ -17,7 +17,7 @@ function Header({ movie }) {
           </h1>
           <div className='top-rated-movie'>
             <div className='rate'>
-              <img src='../assets/images/star_empty.png' className='star' />{' '}
+              <img src='../assets/images/star_empty.png' className='star' />
               <span className='rate-value'>{round(rate, 1)}</span>
             </div>
             <div className='title'>{title}</div>
