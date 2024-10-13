23b958f190ddf981d45e27e91b501c5d219359be
ss0526100 <ss0526100@gmail.com>
2024-10-13 21:51:00 +0900

    feat: 1단계에서 못한 미션 구현

--- src/client/main.js
+++ src/client/main.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { hydrateRoot } from "react-dom/client";
-import App from "./App";
+import App from './App';
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client';
 
-hydrateRoot(document.getElementById("root"), <App />);
+hydrateRoot(document.getElementById('root'), <App movies={movies} />);
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,32 +1,33 @@
-import App from "../../client/App";
-import fs from "fs";
-import path from "path";
-
-import React from "react";
-import { Router } from "express";
-import getMovies from "../../client/apis/getMovies";
-import { renderToString } from "react-dom/server";
+import App from '../../client/App';
+import React from 'react';
+import { Router } from 'express';
+import fs from 'fs';
+import getMovies from '../../client/apis/getMovies';
+import path from 'path';
+import { renderToString } from 'react-dom/server';
 
 const router = Router();
 
-router.use("*", async (_, res) => {
-  // const initData = template.replace(
-  //   "<!--${INIT_DATA_AREA}-->",
-  //   /*html*/ `
-  //   <script>
-  //     window.__INITIAL_DATA__ = {
-  //       movies: ${JSON.stringify(popularMovies)}
-  //     }
-  //   </script>
-  // `
-  // );
-
+router.use('*', async (_, res) => {
   const movies = await getMovies.nowPlaying();
   const renderedApp = renderToString(<App movies={movies} />);
-  const templatePath = path.resolve(__dirname, "index.html");
-  const template = fs.readFileSync(templatePath, "utf8");
+  const templatePath = path.resolve(__dirname, 'index.html');
+  const template = fs.readFileSync(templatePath, 'utf8');
 
-  res.send(template.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`));
+  const result = template
+    .replace(
+      '<!--aaaaa-->',
+      /*html*/ `
+    <script>
+      window.__INITIAL_DATA__ = {
+        movies: ${JSON.stringify(movies)}
+      }
+    </script>
+  `
+    )
+    .replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`);
+
+  res.send(result);
 });
 
 export default router;
