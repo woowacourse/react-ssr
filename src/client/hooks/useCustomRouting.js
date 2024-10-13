157a8920501e8ddf5d7cdfc4738fcbbf4faa28c5
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-10-14 08:32:15 +0900

    refactor: 옵저버 패턴 사용 훅 useSyncExternalStore로 변경
    squash

--- src/client/hooks/useCustomRouting.js
+++ src/client/hooks/useCustomRouting.js
@@ -1,23 +1,19 @@
-import { useEffect, useState } from "react";
+import { useSyncExternalStore } from "react";
 import { matchRoute } from "../utils";
 import URLObserver from "../observer/URLObserver";
 
 const urlObserver = new URLObserver();
 
 export default function useCustomRouting() {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
-
-  useEffect(() => {
-    const listener = (newPath) => {
-      setCurrentPath(newPath);
-    };
-
-    urlObserver.addListener(listener);
-
-    return () => {
-      urlObserver.removeListener(listener);
-    };
-  }, []);
+  const currentPath = useSyncExternalStore(
+    (callback) => {
+      urlObserver.addListener(callback);
+      return () => {
+        urlObserver.removeListener(callback);
+      };
+    },
+    () => window.location.pathname
+  );
 
   return { currentPath, match: matchRoute(currentPath) };
 }
