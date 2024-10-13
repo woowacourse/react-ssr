d95da304bd0ca4ba12693e9048f0a7e1e0fd6e5a
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-10-13 20:27:19 +0900

    feat: 모달 구현

--- src/client/main.js
+++ src/client/main.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { hydrateRoot } from "react-dom/client";
+import { createRoot, hydrateRoot } from "react-dom/client";
 import Movies from "./components/movies";
 import HeaderContent from "./components/HeaderContent";
+import Modal from "./components/Modal";
 
 const INIT_DATA = window.__INITIAL_DATA__;
 
 hydrateRoot(document.getElementById("HEADER"), <HeaderContent movie={INIT_DATA.movies[0]} />);
 hydrateRoot(document.getElementById("MOVIE_ITEMS"), <Movies movies={INIT_DATA.movies} />);
+
+createRoot(document.getElementById("MODAL_AREA")).render(<Modal />);
