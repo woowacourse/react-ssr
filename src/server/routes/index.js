6993b01a7917c46731b6c751a34ecd533bdd2dad
soosoo22 <lllllh00@naver.com>
2024-10-13 23:47:26 +0900

    fix: 뼈대 코드 수정

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,8 @@
-import { Router } from "express";
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 
 import React from "react";
+import { Router } from "express";
 import { renderToString } from "react-dom/server";
 
 import { TMDB_MOVIE_LISTS } from "../api/endpoints.js";
@@ -11,19 +10,15 @@ import { fetchMovieList } from "../api/movies.js";
 
 import App from "../../client/App.jsx";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const router = Router();
 
 router.get("/", async (_, res) => {
   const popularMovies = await fetchMovieList(TMDB_MOVIE_LISTS.NOW_PLAYING);
-
-  const templatePath = path.join(__dirname, "../../../views", "index.html");
-
   const renderedApp = renderToString(<App movies={popularMovies} />);
 
-  const template = fs.readFileSync(templatePath, "utf-8");
+  const templatePath = path.resolve(__dirname, "index.html");
+  const template = fs.readFileSync(templatePath, "utf8");
+
   const initData = template.replace(
     "<!--${INIT_DATA_AREA}-->",
     /*html*/ `
@@ -35,9 +30,7 @@ router.get("/", async (_, res) => {
   `
   );
 
-  const renderedHTML = initData.replace("<!--${CONTENT}-->", renderedApp);
-
-  res.send(renderedHTML);
+  res.send(initData.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`));
 });
 
 export default router;
