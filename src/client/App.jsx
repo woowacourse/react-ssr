8d041c7429475c6b4caab90144bb7c3c73501dc2
YUNAH <61104776+anttiey@users.noreply.github.com>
2024-10-14 12:30:10 +0900

    [서버 사이드 렌더링 - 1단계] 파슬리(김윤아) 미션 제출합니다. (#30)
    * feat: 서버에서 TMDB API 호출하도록 구현
    
    * feat: 영화 목록 컴포넌트 구현
    
    * feat: 헤더 컴포넌트 구현
    
    * refactor: 서버 상수 정리
    
    * refactor: HTML 태그 변경

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-import Home from "./components/Home";
+import React from 'react';
 
-function App() {
+import Header from './components/Header';
+import Home from './components/Home';
+
+function App({ movies }) {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Header bestMovie={movies[0]} />
+      <div className="container">
+        <Home movies={movies} />
+      </div>
+    </>
   );
 }
 
---
+++ src/client/components/MovieItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TMDB_THUMBNAIL_URL = 'https://media.themoviedb.org/t/p/w440_and_h660_face';
+
+function MovieItem({ movie }) {
+  const { title, poster_path, vote_average } = movie;
+
+  return (
+    <li className="item">
+      <img className="thumbnail" src={TMDB_THUMBNAIL_URL + poster_path} alt={title} />
+      <div className="item-desc">
+        <p className="rate">
+          <img src="/assets/images/star_empty.png" className="star" alt="star" />
+          <span>{vote_average.toFixed(1)}</span>
+        </p>
+        <strong>{title}</strong>
+      </div>
+    </li>
+  );
+}
+
+export default MovieItem;
