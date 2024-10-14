f440306be5d54526e49d74aa7ed2ecaa0db296aa
Pakxe <64801796+pakxe@users.noreply.github.com>
2024-10-14 17:23:59 +0900

    [서버 사이드 렌더링 - 1단계] 웨디(박세현) 미션 제출합니다. (#13)

---
+++ src/client/components/Header.jsx
@@ -0,0 +1,34 @@
+import { TMDB_BANNER_URL } from '../constants.js';
+import { round } from '../utils';
+import React from 'react'; // 왜 react import 없으면 안되는거지?
+
+function Header({ movie }) {
+  const { title, rate, background } = movie;
+
+  const bannerUrl = TMDB_BANNER_URL + background;
+
+  return (
+    <header>
+      <div className='background-container' style={{ backgroundImage: `url("${bannerUrl}")` }}>
+        <div className='overlay' aria-hidden='true'></div>
+        <div className='top-rated-container'>
+          <h1 className='logo'>
+            <img src='../assets/images/logo.png' alt='MovieList' />
+          </h1>
+          <div className='top-rated-movie'>
+            <div className='rate'>
+              <img src='../assets/images/star_empty.png' className='star' />
+              <span className='rate-value'>{round(rate, 1)}</span>
+            </div>
+            <div className='title'>{title}</div>
+            <button className='primary detail' onClick={() => alert('클릭')}>
+              자세히 보기
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/client/components/MovieItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { TMDB_THUMBNAIL_URL } from '../constants.js';
+import { round } from '../utils.js';
+
+function MovieItem({ rate, title, background }) {
+  const thumbnailFullUrl = TMDB_THUMBNAIL_URL + '/' + background;
+
+  return (
+    <div className='item'>
+      <img className='thumbnail' src={thumbnailFullUrl} alt={title} />
+      <div className='item-desc'>
+        <p className='rate'>
+          <img src='../assets/images/star_empty.png' className='star' />
+          <span>{round(rate, 1)}</span>
+        </p>
+        <strong>{title}</strong>
+      </div>
+    </div>
+  );
+}
+
+export default MovieItem;
