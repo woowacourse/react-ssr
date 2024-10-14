903a7ad26211f54a7f7288ace66135b813d0b2f1
rbgksqkr <rbgks1937@naver.com>
2024-10-14 21:04:44 +0900

    refactor: MovieDetailModal 구현 및 MovieDetailPage 분리

---
+++ src/client/pages/MovieDetailPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MovieDetailModal from "../components/MovieDetailModal";
+
+const MovieDetailPage = () => {
+  return <MovieDetailModal />;
+};
+
+export default MovieDetailPage;
