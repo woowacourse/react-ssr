960a6bfee01509b977c1fd78d4bbe747060210db
Largopie <jae990320@kakao.com>
2024-10-15 01:36:01 +0900

    feat: 영화 상세정보 모달 컴포넌트 구현

---
+++ src/client/components/MovieDetailModal.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function MovieDetailModal({ movie }) {
+  const { title, thumbnail, releaseYear, genres, rate, description } = movie;
+
+  return (
+    <div className="modal-background active">
+      <div className="modal">
+        <button className="close-modal">
+          <img src="./static/images/modal_button_close.png" />
+        </button>
+        <div className="modal-container">
+          {movie ? (
+            <>
+              <div className="modal-image">
+                <img src={`https://image.tmdb.org/t/p/original${thumbnail}`} />
+              </div>
+              <div className="modal-description">
+                <h2>{title}</h2>
+                <p className="category">
+                  {releaseYear} · {genres.join(", ")}
+                </p>
+                <p className="rate">
+                  <img src="./static/images/star_empty.png" className="star" />
+                  <span>{rate}</span>
+                </p>
+                <hr />
+                <p className="detail">{description}</p>
+              </div>
+            </>
+          ) : (
+            "영화 정보를 찾을 수 없습니다."
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
