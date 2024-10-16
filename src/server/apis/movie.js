c18e61f27f6532323187a44209df38d466a77edc
rbgksqkr <rbgks1937@naver.com>
2024-10-16 16:46:56 +0900

    feat: movieDetail API 요청 함수 구현

--- src/server/apis/movie.js
+++ src/server/apis/movie.js
@@ -1,7 +1,22 @@
-import { FETCH_OPTIONS, TMDB_MOVIE_LISTS } from "../constants/index.js";
+import {
+  FETCH_OPTIONS,
+  TMDB_MOVIE_DETAIL_URL,
+  TMDB_MOVIE_LISTS,
+} from "../constants/index.js";
 
 export const fetchMoviesPopular = async () => {
   const response = await fetch(TMDB_MOVIE_LISTS.POPULAR, FETCH_OPTIONS);
+
+  return await response.json();
+};
+
+export const fetchMovieDetail = async (movieId) => {
+  const url = TMDB_MOVIE_DETAIL_URL + movieId;
+  const params = new URLSearchParams({
+    language: "ko-KR",
+  });
+
+  const response = await fetch(url + "?" + params, FETCH_OPTIONS);
 
   return await response.json();
 };
