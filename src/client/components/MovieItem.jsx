186971e18558a9ba151da8b7c3d9f379341f2305
badahertz52 <badahertz52@gmail.com>
2024-10-16 15:56:45 +0900

    feat: MovieItem에 클릭 시 모달 여는 이벤트 추가

--- src/client/components/MovieItem.jsx
+++ src/client/components/MovieItem.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import { round } from "../utils";
 import { TMDB_THUMBNAIL_URL } from "../constants";
 import starEmptyImage from "@images/star_empty.png";
+import { useNavigate } from "react-router-dom";
 
-function MovieItem({ rate, title, thumbnailUrl, onClick }) {
+function MovieItem({ id, rate, title, thumbnailUrl, onClick }) {
+  const navigate = useNavigate();
+
   const thumbnailFullUrl = TMDB_THUMBNAIL_URL + "/" + thumbnailUrl;
 
+  const handleMovieItemClick = () => {
+    navigate(`/detail/${id}`);
+  };
+
   return (
-    <div className="item" onClick={onClick}>
+    <div className="item" onClick={handleMovieItemClick}>
       <img className="thumbnail" src={thumbnailFullUrl} alt={title} />
       <div className="item-desc">
         <p className="rate">
