1a7acfe231c6c266d5a5b0986d13a2df05ef6100
jinsil <jinsil378900@gmail.com>
2024-10-16 16:07:38 +0900

    refactor: index.html의 주석 제거

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,8 +4,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { renderToString } from "react-dom/server";
 import React from "react";
-import { fetchNowPlayingMovies } from "../api";
+import { fetchMovies } from "../api";
 import App from "../../client/App";
+import { TMDB_MOVIE_LISTS } from "../../constant";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,25 +14,21 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 router.use("/", async (_, res) => {
-  const movies = await fetchNowPlayingMovies();
+  const movies = await fetchMovies(TMDB_MOVIE_LISTS.NOW_PLAYING);
 
   const templatePath = path.join(__dirname, "../../../views", "index.html");
   const template = fs.readFileSync(templatePath, "utf-8");
 
   const renderedApp = renderToString(<App movies={movies.results} />);
-
-  const initData = template.replace(
-    "<!--${INIT_DATA_AREA}-->",
-    /*html*/ `
+  const initData = /*html*/ `
     <script>
       window.__INITIAL_DATA__ = {
         movies: ${JSON.stringify(movies)}
       }
     </script>
-  `
-  );
+  `;
 
-  res.send(initData.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`));
+  res.send(template.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>${initData}`));
 });
 
 export default router;
