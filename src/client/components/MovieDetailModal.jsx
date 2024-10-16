da2aa3e1467f02cdabbf606576756e56eb7bf40d
rbgksqkr <rbgks1937@naver.com>
2024-10-16 16:44:18 +0900

    refactor: MovieDetailModal 컴포넌트 데이터 인자로 받아 렌더링

--- src/client/components/MovieDetailModal.jsx
+++ src/client/components/MovieDetailModal.jsx
@@ -1,10 +1,38 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 
-const MovieDetailModal = () => {
-  const { movieId } = useParams();
+const MovieDetailModal = ({ movieDetail }) => {
+  const { title, releaseYear, genres, rate, description, bannerUrl } =
+    movieDetail;
 
-  return <div>MovieDetailModal : {movieId}</div>;
+  return (
+    <div className="modal-background active" id="modalBackground">
+      <div className="modal">
+        <button className="close-modal" id="closeModal">
+          <img src="/static/images/modal_button_close.png" />
+        </button>
+        <div className="modal-container">
+          <div className="modal-image">
+            <img
+              src={`https://media.themoviedb.org/t/p/w440_and_h660_face/${bannerUrl}`}
+              alt="영화 포스터"
+            />
+          </div>
+          <div className="modal-description">
+            <h2>{title}</h2>
+            <p className="category">
+              {releaseYear} · {genres}
+            </p>
+            <p className="rate">
+              <img src="/static/images/star_empty.png" className="star" />
+              <span>{rate}</span>
+            </p>
+            <hr />
+            <p className="detail">{description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetailModal;
