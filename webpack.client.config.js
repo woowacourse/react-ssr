528acf9b0c9fb33e09fa56d66302b033c43cd732
chysis <chysiss@naver.com>
2024-10-17 22:01:27 +0900

    fix: process is not defined 오류 수정

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
@@ -47,6 +48,9 @@ module.exports = {
         { from: 'public/images', to: 'images' },
         { from: 'public/styles', to: 'styles' },
       ],
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
     }),
   ],
   resolve: {
