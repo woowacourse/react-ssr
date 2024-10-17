b467598f0e5c25e593367cbd758a079c15d55596
ss0526100 <ss0526100@gmail.com>
2024-10-17 22:14:13 +0900

    feat(client/main.js):react router 추가

--- src/client/main.js
+++ src/client/main.js
@@ -1,5 +1,25 @@
-import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './components/Home';
 import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
 
-hydrateRoot(document.getElementById('root'), <App movies={movies} />);
+const { movies, movieDetail } = window.__INITIAL_DATA__;
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home movies={movies} />,
+    children: [
+      {
+        path: ':moiveId',
+        element: <Home movies={movies} movieDetail={movieDetail} />,
+      },
+    ],
+  },
+]);
+
+hydrateRoot(
+  document.getElementById('root'),
+  <RouterProvider router={router} />
+);
