5f362d1ff3162857fcad21d85e07d99049640ebc
손진영 <sm099@naver.com>
2024-10-17 16:49:21 +0900

    feat(routes/index): 영화 상세 정보 페이지에 대한 route 로직 추가
    1. renderMovieDetailHTML 함수 추가
    
    2. /detail/:id 라우트에 대한 ssr 로직 추가

--- src/server/utils/render.js
+++ src/server/utils/render.js
@@ -5,7 +5,7 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 
-import { fetchNowPlayingMovies } from '../apis/movies.js';
+import { fetchMovieDetail, fetchNowPlayingMovies } from '../apis/movies.js';
 
 import App from '../../client/App.jsx';
 
@@ -23,7 +23,7 @@ export async function renderRootHTML() {
 
   const renderedApp = renderToString(
     <StaticRouter location={'/'}>
-      <App movies={nowPlayingMovies} />
+      <App initialMovieDetail={null} movies={nowPlayingMovies} />
     </StaticRouter>
   );
 
@@ -33,6 +33,31 @@ export async function renderRootHTML() {
       <script>
         window.__INITIAL_DATA__ = {
           movies: ${JSON.stringify(nowPlayingMovies)}
+        }
+      </script>
+    `
+  );
+}
+
+export async function renderMovieDetailHTML(movieId) {
+  const template = createHTMLTemplate();
+
+  const nowPlayingMovies = await fetchNowPlayingMovies();
+  const movieDetail = await fetchMovieDetail(movieId);
+
+  const renderedApp = renderToString(
+    <StaticRouter location={`/detail/${movieId}`}>
+      <App initialMovieDetail={movieDetail} movies={nowPlayingMovies} />
+    </StaticRouter>
+  );
+
+  return template.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`).replace(
+    '<!--${INIT_DATA_AREA}-->',
+    `
+      <script>
+        window.__INITIAL_DATA__ = {
+          movies: ${JSON.stringify(nowPlayingMovies)},
+          movie : ${JSON.stringify(movieDetail)},
         }
       </script>
     `
