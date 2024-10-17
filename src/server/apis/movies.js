483a26abf44ba92f1db68555260315a3a552952a
손진영 <sm099@naver.com>
2024-10-17 16:48:18 +0900

    feat: 영화 상세 정보에 대한 api 함수 추가
    1. fetchMovieList에서 movieFetcher로 변경
    
    2. addQuaryParams 함수 추가
    
    3. FETCH_OPTIONS 분리
    
    4. fetchNowPlayingMovies 형태 변경
    
    5. fetchMovieDetail 추가

--- src/server/apis/movies.js
+++ src/server/apis/movies.js
@@ -1,24 +1,53 @@
-import { TMDB_BANNER_URL, TMDB_MOVIE_LISTS, TMDB_THUMBNAIL_URL } from './url.js';
+import {
+  TMDB_BANNER_URL,
+  TMDB_MOVIE_DETAIL_URL,
+  TMDB_MOVIE_LISTS,
+  TMDB_THUMBNAIL_URL,
+} from './url.js';
 
-const fetchMovieList = async (type) => {
-  const response = await fetch(TMDB_MOVIE_LISTS[type || 'nowPlaying'], {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
-    },
-  });
+import { addQueryParams } from '../utils/query.js';
+
+const FETCH_OPTIONS = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
+  },
+};
+
+const movieFetcher = async (url) => {
+  const urlWithQuery = addQueryParams(url, { language: 'ko-KR' });
+  const response = await fetch(urlWithQuery, FETCH_OPTIONS);
 
   return await response.json();
 };
 
 export async function fetchNowPlayingMovies() {
-  const data = await fetchMovieList('nowPlaying');
+  const data = await movieFetcher(TMDB_MOVIE_LISTS['nowPlaying']);
 
-  return data.results.map(({ title, vote_average, backdrop_path, poster_path }) => ({
+  return data.results.map(({ id, title, vote_average, backdrop_path, poster_path }) => ({
+    id,
     title,
     rate: vote_average.toFixed(1),
     backdropUrl: `${TMDB_BANNER_URL}/${backdrop_path}`,
     thumbnailUrl: `${TMDB_THUMBNAIL_URL}${poster_path}`,
   }));
+}
+
+export async function fetchMovieDetail(movieId) {
+  const movieDetailUrl = `${TMDB_MOVIE_DETAIL_URL}${movieId}`;
+
+  const { title, vote_average, backdrop_path, poster_path, genres, release_date, overview } =
+    await movieFetcher(movieDetailUrl);
+
+  const releaseYear = release_date.slice(0, 4);
+
+  return {
+    title,
+    releaseYear,
+    rate: vote_average.toFixed(1),
+    thumbnailUrl: `${TMDB_THUMBNAIL_URL}${poster_path}`,
+    genres: genres.map((genre) => genre.name).join(', '),
+    description: overview,
+  };
 }
