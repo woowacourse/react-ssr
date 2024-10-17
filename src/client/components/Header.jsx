a43d91b63abda413821f087b7acac1f493a48588
chysis <chysiss@naver.com>
2024-10-18 00:52:35 +0900

    feat: 클릭 navigate 동작 추가

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import starEmptyImage from '@images/star_empty.png';
+import StarEmptyImage from '@images/star_empty.png';
 import LogoImage from '@images/logo.png';
 import { TMDB_BANNER_URL } from '../../server/constants/movies';
+import { useNavigate } from 'react-router-dom';
 
 const Header = ({ bestMovie }) => {
+  const navigate = useNavigate();
+
   const backgroundContainer = TMDB_BANNER_URL + '/' + bestMovie.backdrop_path;
   const bestMovieRate = bestMovie.vote_average.toFixed(1);
   const bestMovieTitle = bestMovie.title;
@@ -18,11 +21,13 @@ const Header = ({ bestMovie }) => {
           </h1>
           <div className='top-rated-movie'>
             <div className='rate'>
-              <img src={starEmptyImage} className='star' />
+              <img src={StarEmptyImage} className='star' />
               <span className='rate-value'>{bestMovieRate}</span>
             </div>
             <div className='title'>{bestMovieTitle}</div>
-            <button className='primary detail'>자세히 보기</button>
+            <button className='primary detail' onClick={() => navigate(`/detail/${bestMovie.id}`)}>
+              자세히 보기
+            </button>
           </div>
         </div>
       </div>
