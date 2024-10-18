f23a84aa86b8077690da70ea3adcd15b6142066a
jaeml06 <jaeml0630@gmail.com>
2024-10-18 10:38:00 +0900

    feat: 라우터 설정 추가

--- src/client/main.js
+++ src/client/main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const initialData = window.__INITIAL_DATA__ || [];
-hydrateRoot(document.getElementById("root"), <App bestMovie={initialData.movies[0]} movies={initialData.movies} />);
+hydrateRoot(document.getElementById("root"),  <BrowserRouter><App movies={initialData.movies} movieDetail={initialData.movieDetail} /></BrowserRouter>);
