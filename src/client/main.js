4e4d1c970110232250f5949f19cf90d860228364
simhorim <hori5020@gmail.com>
2024-10-18 19:21:04 +0900

    feat(main): home, detail 페이지에 대한 라우팅 기능 구현

--- src/client/main.js
+++ src/client/main.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const movies = window.__INITIAL_DATA__.movies;
+const initialMovies = window.__INITIAL_DATA__.movies;
+const initialMovie = window.__INITIAL_DATA__.movie;
 
-hydrateRoot(document.getElementById("root"), <App movies={movies} />);
+hydrateRoot(
+  document.getElementById("root"),
+  <BrowserRouter>
+    <App initialMovies={initialMovies} initialMovie={initialMovie} />
+  </BrowserRouter>
+);
