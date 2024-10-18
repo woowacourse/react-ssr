8c894083448a173a214a3567f6278ee8440a2838
simhorim <hori5020@gmail.com>
2024-10-18 19:22:43 +0900

    feat(webpack.client): dot env 사용하도록 구현

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 module.exports = {
   mode: "development",
@@ -47,6 +51,9 @@ module.exports = {
         { from: "public/images", to: "images" }, // public 폴더의 이미지를 dist로 복사
         { from: "public/styles", to: "styles" },
       ],
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
     }),
   ],
   resolve: {
