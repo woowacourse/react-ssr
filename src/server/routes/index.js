8fd097c9f75db8525c1d0ca338f8a979cd68c3a7
chysis <chysiss@naver.com>
2024-10-20 00:08:07 +0900

    feat: 서버 라우터 설정

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,24 +3,61 @@ import fs from 'fs';
 import path from 'path';
 
 import React from 'react';
-import { renderToString } from 'react-dom/server';
-import App from '../../client/App';
-import fetchNowPlayingMovies from '../apis/movies.js';
+import * as ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import { fetchMovieDetail, fetchNowPlayingMovies } from './../../client/apis/movies';
+import MovieDetail from '../../client/pages/MovieDetail.jsx';
+import MoviePage from '../../client/pages/MoviePage.jsx';
 
 const router = Router();
 
-router.use('/', async (_, res) => {
+router.get('/', async (req, res) => {
   const nowPlayingMovies = await fetchNowPlayingMovies();
 
   const templatePath = path.resolve(__dirname, 'index.html');
-  const renderedApp = renderToString(<App movies={nowPlayingMovies} />);
-
   const template = fs.readFileSync(templatePath, 'utf-8');
+
+  const initialData = { movies: nowPlayingMovies };
+
+  const renderedApp = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url}>
+      <MoviePage movies={initialData.movies} />
+    </StaticRouter>
+  );
 
   const initData = /*html*/ `
     <script>
       window.__INITIAL_DATA__ = {
         movies: ${JSON.stringify(nowPlayingMovies)}
+      }
+    </script>
+  `;
+
+  res.send(template.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>${initData}`));
+});
+
+router.get('/detail/:id', async (req, res) => {
+  const movieId = req.params.id;
+  const nowPlayingMovies = await fetchNowPlayingMovies();
+  const movieDetail = await fetchMovieDetail(movieId);
+
+  const templatePath = path.resolve(__dirname, 'index.html');
+  const template = fs.readFileSync(templatePath, 'utf-8');
+
+  const initialData = { movies: nowPlayingMovies, movieDetail: movieDetail };
+
+  const renderedApp = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url}>
+      <MoviePage movies={initialData.movies} />
+      <MovieDetail detail={initialData.movieDetail} />
+    </StaticRouter>
+  );
+
+  const initData = /*html*/ `
+    <script>
+      window.__INITIAL_DATA__ = {
+        movies: ${JSON.stringify(nowPlayingMovies)},
+        movieDetail: ${JSON.stringify(movieDetail)}
       }
     </script>
   `;
