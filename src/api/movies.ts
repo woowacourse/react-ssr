a48185afb413f9ca35c2ed790850c146f0d1dace
jayming66 <mjw8941@gmail.com>
2024-10-19 15:40:14 +0900

    feat: movieDetail Fetch 함수

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,5 +1,6 @@
 const BASE_URL = 'https://api.themoviedb.org/3/movie';
 const POPULAR = BASE_URL + '/popular?language=ko-KR&page=1';
+
 const FETCH_OPTIONS = {
   method: 'GET',
   headers: {
@@ -11,6 +12,18 @@ const FETCH_OPTIONS = {
 export const fetchMovies = async () => {
   const response = await fetch(POPULAR, FETCH_OPTIONS);
   const results = await response.json().then((data) => data.results);
+
+  return results;
+};
+
+export const fetchMovieDetail = async (id) => {
+  const params = new URLSearchParams({
+    language: 'ko-KR',
+  });
+  const url = `${BASE_URL}/${id}?${params}`;
+
+  const response = await fetch(url, FETCH_OPTIONS);
+  const results = await response.json().then((data) => data);
 
   return results;
 };
