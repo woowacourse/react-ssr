6c689398c466833d8a92188d4fc24be032bbc1c5
jayming66 <mjw8941@gmail.com>
2024-10-19 15:54:42 +0900

    feat: /detail/{movieId} 라우트 생성

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,21 +1,41 @@
-import { Router } from 'express';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { Router } from "express";
+import fs from "fs";
+import path from "path";
 
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import App from '../../client/App';
-import { fetchMovies } from '../../api/movies.ts';
+import React from "react";
+import { renderToString } from "react-dom/server";
+import App from "../../client/App";
+import { fetchMovieDetail, fetchMovies } from "../../api/movies.ts";
 
 const router = Router();
 
-router.use('/', async (_, res) => {
+router.get("/", async (_, res) => {
   const popularMovies = await fetchMovies();
 
   const renderedApp = renderToString(<App movies={popularMovies} />);
-  const templatePath = path.resolve(__dirname, 'index.html');
-  const template = fs.readFileSync(templatePath, 'utf8');
+  const templatePath = path.resolve(__dirname, "index.html");
+  const template = fs.readFileSync(templatePath, "utf8");
+
+  res.send(
+    template.replace(
+      '<div id="root"></div>',
+      `<div id="root">${renderedApp}</div>
+      <script>window.__INITIAL_DATA__ = ${JSON.stringify({
+        popularMovies,
+      })}</script>`
+    )
+  );
+});
+
+router.get("/detail/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const popularMovies = await fetchMovies();
+  // const movieDetail = await fetchMovieDetail(id);
+
+  const renderedApp = renderToString(<App movies={popularMovies} />);
+  const templatePath = path.resolve(__dirname, "index.html");
+  const template = fs.readFileSync(templatePath, "utf8");
 
   res.send(
     template.replace(
