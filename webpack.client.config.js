c66d8579235f39e6aaf4832792378c9da6491531
jayming66 <mjw8941@gmail.com>
2024-10-19 21:23:21 +0900

    chore: dotenv-webpack 설치

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,15 +1,16 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/client/main.js',
+  mode: "development",
+  entry: "./src/client/main.js",
   output: {
-    path: path.resolve('dist'),
-    filename: 'bundle.js',
+    path: path.resolve("dist"),
+    filename: "bundle.js",
     clean: true,
-    publicPath: '/static/',
+    publicPath: "/static/",
   },
   module: {
     rules: [
@@ -17,43 +18,44 @@ module.exports = {
         test: /\.(jsx?)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'images/[name][ext]',
+          filename: "images/[name][ext]",
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './views/index.html',
-      filename: 'index.html',
-      inject: 'body',
+      template: "./views/index.html",
+      filename: "index.html",
+      inject: "body",
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'public/images', to: 'images' },
-        { from: 'public/styles', to: 'styles' },
+        { from: "public/images", to: "images" },
+        { from: "public/styles", to: "styles" },
       ],
     }),
+    new Dotenv(),
   ],
   resolve: {
     alias: {
-      '@images': path.resolve(__dirname, 'public/images'),
-      '@styles': path.resolve(__dirname, 'public/styles'),
+      "@images": path.resolve(__dirname, "public/images"),
+      "@styles": path.resolve(__dirname, "public/styles"),
     },
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
 };
