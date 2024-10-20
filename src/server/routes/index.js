f02c1efe44f328ddcdab24b84e8c001742580bd1
river <130737187+0jenn0@users.noreply.github.com>
2024-10-21 03:41:52 +0900

    feat(routes): 서버측 route에 영화 상세 정보 전달 추가
    - 서버 라우팅 경로를 "*"로 변경하여 모든 경로 처리
    - 영화 상세 정보(movieDetail) 데이터 전달 로직 추가
    - StaticRouter 적용으로 서버 사이드 라우팅 구현

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,28 +1,40 @@
 import { Router } from "express";
 import fs from "fs";
 import path from "path";
-
 import React from "react";
 import { renderToString } from "react-dom/server";
-import { fetchMovies } from "../utils/tmdb";
+import { StaticRouter } from "react-router-dom/server";
+import { fetchMovie, fetchMovies } from "../utils/tmdb";
 import App from "../../client/App";
 
 const router = Router();
 
-router.use("/", async (_, res) => {
+router.get("*", async (req, res) => {
+  const movies = await fetchMovies("nowPlaying");
+  let movieDetail = null;
+
+  const id = req.path.split("/detail/")[1];
+
+  if (id) {
+    movieDetail = await fetchMovie(id);
+  }
+
+  const html = renderToString(
+    <StaticRouter location={req.url}>
+      <App movies={movies} movieDetail={movieDetail} />
+    </StaticRouter>
+  );
+
   const templatePath = path.resolve(__dirname, "index.html");
   const template = fs.readFileSync(templatePath, "utf8");
-
-  const movies = await fetchMovies("nowPlaying");
-
-  const renderedApp = renderToString(<App movies={movies} />);
 
   res.send(
     template.replace(
       '<div id="root"></div>',
-      `<div id="root">${renderedApp}</div>
+      `<div id="root">${html}</div>
       <script>window.__INITIAL_DATA__ = ${JSON.stringify({
         movies,
+        movieDetail,
       })}</script>`
     )
   );
