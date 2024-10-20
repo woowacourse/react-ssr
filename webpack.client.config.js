58bc008673d21f570799bfa0dc4864f6a6412a17
useon <roqkftjs@gmail.com>
2024-10-21 05:06:14 +0900

    chore: 클라이언트 env 웹팩 설정

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
   mode: 'development',
@@ -47,6 +50,9 @@ module.exports = {
         { from: 'public/images', to: 'images' },
         { from: 'public/styles', to: 'styles' },
       ],
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
     }),
   ],
   resolve: {
