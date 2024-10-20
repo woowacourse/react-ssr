5cf29416c1e65a30f2e5f8018fe54c61406409ab
jinsil <jinsil378900@gmail.com>
2024-10-21 02:12:41 +0900

    fix: get을 use로 변경

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,4 +1,3 @@
-import App from "../../client/App";
 import fs from "fs";
 import path from "path";
 import React from "react";
@@ -12,14 +11,14 @@ import App from "../../client/App";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.use("/", async (req, res) => {
   const movies = await fetchMovies(TMDB_MOVIE_LISTS.NOW_PLAYING);
 
   const templatePath = path.resolve(__dirname, "index.html");
   const template = fs.readFileSync(templatePath, "utf-8");
 
   const renderedApp = renderToString(
-    <StaticRouter location={req.url}>
+    <StaticRouter location={"/"}>
       <App
         movies={movies.results}
         movieDetailItem={null}
@@ -38,7 +37,7 @@ router.get("/", async (req, res) => {
   res.send(template.replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>${initData}`));
 });
 
-router.get("/detail/:id", async (req, res) => {
+router.use("/detail/:id", async (req, res) => {
   const movies = await fetchMovies(TMDB_MOVIE_LISTS.NOW_PLAYING);
   const movieId = req.params.id;
   const movieDetailItem = await fetchMovieDetail(movieId);
@@ -47,7 +46,7 @@ router.get("/detail/:id", async (req, res) => {
   const template = fs.readFileSync(templatePath, "utf-8");
 
   const renderedApp = renderToString(
-    <StaticRouter location={req.url}>
+    <StaticRouter location={`/detail/${movieId}`}>
       <App
         movies={movies.results}
         movieDetailItem={movieDetailItem}
