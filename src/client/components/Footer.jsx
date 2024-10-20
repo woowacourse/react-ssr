1b3f298c277f0b72fcaf0d5855fc428f5d6a5531
Seongjin Hong <hongseongjin.to@gmail.com>
2024-10-20 23:59:16 +0900

    fix: Footer 컴포넌트의 JSX 내 class 표기법 수정

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer class="footer">
+    <footer className="footer">
       <p>&copy; 우아한테크코스 All Rights Reserved.</p>
       <p>
         <img src="../static/images/woowacourse_logo.png" width="180" />
