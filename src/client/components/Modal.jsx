a691ec1b43dbe4b479b51785c04d796c745b8439
useon <roqkftjs@gmail.com>
2024-10-21 03:12:54 +0900

    feat: 모달 구현

---
+++ src/client/components/Modal.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ModalButtonClose from '@images/modal_button_close.png';
+import StarFilled from '@images/star_filled.png';
+
+const Modal = ({ movieDetail }) => {
+  return (
+    <div className='modal-background active' id='modalBackground'>
+      <div className='modal'>
+        <button className='close-modal' id='closeModal'>
+          <img src={ModalButtonClose} />
+        </button>
+        <div className='modal-container'>
+          <div className='modal-image'>
+            <img src={movieDetail.poster_path} />
+          </div>
+          <div className='modal-description'>
+            <h2>{movieDetail.title}</h2>
+            <p className='category'>
+              {movieDetail.releaseYear} · {movieDetail.genres.join(', ')}
+            </p>
+            <p className='rate'>
+              <img src={StarFilled} className='star' />
+              <span>7.7</span>
+            </p>
+            <hr />
+            <p className='detail'>{movieDetail.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
