398db0a991fd9cdeeb40f083405e99451bc78ebc
ss0526100 <ss0526100@gmail.com>
2024-10-20 17:01:36 +0900

    feat:모달 url 라우팅 구현

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,18 +1,57 @@
-import App from '../../client/App';
+import Home from '../../client/components/Home';
 import React from 'react';
 import { Router } from 'express';
+import { StaticRouter } from 'react-router-dom/server';
 import fs from 'fs';
+import getMovieDetail from '../../client/apis/getMovieDetail';
 import getMovies from '../../client/apis/getMovies';
 import path from 'path';
 import { renderToString } from 'react-dom/server';
 
 const router = Router();
 
-router.use('*', async (_, res) => {
+const templatePath = path.resolve(__dirname, 'index.html');
+const template = fs.readFileSync(templatePath, 'utf8');
+
+router.use('/details/:movieId', async (req, res) => {
+  const id = req.params.movieId;
+
   const movies = await getMovies.nowPlaying();
-  const renderedApp = renderToString(<App movies={movies} />);
-  const templatePath = path.resolve(__dirname, 'index.html');
-  const template = fs.readFileSync(templatePath, 'utf8');
+  const movieDetail = await getMovieDetail(id);
+  const renderedApp = renderToString(
+    <StaticRouter location={req.url}>
+      <Home movies={movies} movieDetail={movieDetail} />
+    </StaticRouter>
+  );
+
+  const result = template
+    .replace(
+      '<!--aaaaa-->',
+      /*html*/ `
+    <script>
+      window.__INITIAL_DATA__ = {
+        movies: ${JSON.stringify(movies)},
+        movieDetail: ${JSON.stringify(movieDetail)}
+      }
+    </script>
+  `
+    )
+    .replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`)
+    .replace(
+      'href="./static/styles/index.css"',
+      'href="../static/styles/index.css"'
+    );
+
+  res.send(result);
+});
+
+router.use('*', async (req, res) => {
+  const movies = await getMovies.nowPlaying();
+  const renderedApp = renderToString(
+    <StaticRouter location={req.url}>
+      <Home movies={movies} />
+    </StaticRouter>
+  );
 
   const result = template
     .replace(
