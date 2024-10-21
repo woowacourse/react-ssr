33337604588cc0956d576f8388ffb6842672bc49
hwinkr <dnddl8280@naver.com>
2024-10-21 14:26:52 +0900

    chore: delete unused file

--- src/client/constants/api.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = "https://api.themoviedb.org/3/movie";
-export const TMDB_THUMBNAIL_URL =
-  "https://media.themoviedb.org/t/p/w440_and_h660_face";
-export const TMDB_BANNER_URL =
-  "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces";
