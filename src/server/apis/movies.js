163716c4cc7c1f10d3ffde6eac52f903ca44db63
00kang <kdb070970@gmail.com>
2024-10-21 16:53:57 +0900

    feat: fetchDetailMovie 함수 추가

--- src/server/apis/movies.js
+++ src/server/apis/movies.js
@@ -1,8 +1,16 @@
-import { TMDB_MOVIE_LISTS, FETCH_OPTIONS } from "../constants.js";
+import { TMDB_MOVIE_LISTS, TMDB_MOVIE_DETAIL_URL, FETCH_OPTIONS } from "../constants.js";
 
 export const fetchMovies = async () => {
   const response = await fetch(TMDB_MOVIE_LISTS.nowPlaying, FETCH_OPTIONS);
   const data = await response.json();
 
   return data.results;
+};
+
+export const fetchDetailMovie = async (id) => {
+  const url = TMDB_MOVIE_DETAIL_URL + id;
+  const response = await fetch(url, FETCH_OPTIONS);
+  const data = await response.json();
+
+  return data;
 };
