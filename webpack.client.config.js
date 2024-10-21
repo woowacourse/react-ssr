71c12fc733debed22226f7e96ccf53342d0fd61d
hwinkr <dnddl8280@naver.com>
2024-10-21 14:29:58 +0900

    chore: 클라이언트 웹팩 설정 파일에 Dotenv 플러그인 추가

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -48,6 +49,7 @@ module.exports = {
         { from: "public/styles", to: "styles" },
       ],
     }),
+    new Dotenv(),
   ],
   resolve: {
     alias: {
