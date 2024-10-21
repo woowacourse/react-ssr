5f166287e081e1915777e66ce8b9fbdcbc397b04
jaeml06 <jaeml0630@gmail.com>
2024-10-21 17:34:45 +0900

    feat: 자세히 보기 이벤트 추가

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import { TMDB_BANNER_URL } from "../../../server/constants";
 import starEmptyImage from "@images/star_empty.png";
 import logo from "@images/logo.png";
+import { useLocation, useNavigate } from "react-router-dom";
 function Header({ bestMovie }) {
+  const navigate = useNavigate(); 
+  const location = useLocation();
+
+  const handleDetailClick = () => {
+    navigate(`/detail/${bestMovie.id}`, {
+      state: { backgroundLocation: location },
+    });
+  };
   return (
     <header>
       <div className="background-container" style={{ backgroundImage: `url(${TMDB_BANNER_URL + bestMovie.backdrop_path})` }}>
@@ -17,7 +26,7 @@ function Header({ bestMovie }) {
               <span className="rate-value">{bestMovie.vote_average.toFixed(1)}</span>
             </div>
             <div className="title">{bestMovie.title}</div>
-            <button className="primary detail">자세히 보기</button>
+            <button className="primary detail" onClick={handleDetailClick}>자세히 보기</button>
           </div>
         </div>
       </div>
