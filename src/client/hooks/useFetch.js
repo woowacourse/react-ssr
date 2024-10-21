4613e64d8ccaf778a65527f5b18de5b10f358edc
Largopie <jae990320@kakao.com>
2024-10-21 14:43:30 +0900

    feat: useFetch 구현

---
+++ src/client/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (initialData, fetchCallback) => {
+  const [data, setData] = useState(initialData);
+  const [isPending, setIsPending] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsPending(true);
+        const responseData = await fetchCallback();
+
+        setData(responseData);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsPending(false);
+      }
+    };
+
+    if (typeof window !== "undefined" && !window.__INITIAL_DATA__) {
+      getData();
+    }
+  }, []);
+
+  return { data, isPending, isError };
+};
+
+export default useFetch;
