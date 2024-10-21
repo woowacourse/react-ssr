8576d89055007aa6b8b14050b68887706ece8280
hwinkr <dnddl8280@naver.com>
2024-10-21 14:57:57 +0900

    refactor: react-router-dom/server가 제공해 주는 모듈을 사용해서 서버사이드 렌더링 구현
    - createStaticHandler, createFetchRequest, createStaticRouter를 활용해서 서버측 라우팅 구현
    - StaticRouterProvider를 사용해서 HTML 내용을 채움

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,35 +1,45 @@
-import App from "../../client/App";
+import React from "react";
+
+import { renderToString } from "react-dom/server";
+import {
+  createStaticHandler,
+  createStaticRouter,
+  StaticRouterProvider,
+} from "react-router-dom/server";
+
+import { Router } from "express";
+
 import fs from "fs";
 import path from "path";
 
-import React from "react";
-import { Router } from "express";
-import { renderToString } from "react-dom/server";
-import { MOVIE_PAGE_PATH, MOVIE_TYPE_KEY } from "../constants/path";
-import { fetchMovies } from "../apis/movie";
+import createFetchRequest from "../utils/createFetchRequest";
+
+import { routes } from "../../common/router";
+import { MOVIE_PAGE_PATH } from "../../common/constants/path";
 
 const router = Router();
 
-router.use(MOVIE_PAGE_PATH.home, async (req, res) => {
-  const movies = await fetchMovies(MOVIE_TYPE_KEY[req.path]);
+router.use(MOVIE_PAGE_PATH.all, async (req, res) => {
+  const handler = createStaticHandler(routes);
+  const { query, dataRoutes } = handler;
 
-  const renderedApp = renderToString(<App movies={movies} />);
+  const fetchRequest = createFetchRequest(req, res);
+  const context = await query(fetchRequest);
+
+  const router = createStaticRouter(dataRoutes, context);
+
+  const renderedApp = renderToString(
+    <StaticRouterProvider router={router} context={context} />
+  );
+
   const templatePath = path.resolve(__dirname, "index.html");
   const template = fs.readFileSync(templatePath, "utf8");
 
   res.send(
-    template
-      .replace('<div id="root"></div>', `<div id="root">${renderedApp}</div>`)
-      .replace(
-        "<!--${INIT_DATA_AREA}-->",
-        /*html*/ `
-      <script>
-        window.__INITIAL_DATA__ = {
-          movies: ${JSON.stringify(movies)}
-        }
-      </script>
-    `
-      )
+    template.replace(
+      '<div id="root"></div>',
+      `<div id="root">${renderedApp}</div>`
+    )
   );
 });
 
