c035fa80b7992eadbbcfab1ea1f7d890910036a3
pakxe <pigkill40@naver.com>
2024-10-21 14:46:42 +0900

    feat: MovieItem을 클릭했을 때 detail path로 이동하도록 기능 추가

--- src/client/components/MovieItem.jsx
+++ src/client/components/MovieItem.jsx
@@ -3,19 +3,25 @@ import React from 'react';
 import { TMDB_THUMBNAIL_URL } from '../constants.js';
 import { round } from '../utils.js';
 import StarEmptyImage from '@images/star_empty.png';
+import { useNavigate } from 'react-router-dom';
 
-function MovieItem({ rate, title, background }) {
-  const thumbnailFullUrl = TMDB_THUMBNAIL_URL + '/' + background;
+function MovieItem(movie) {
+  const thumbnailFullUrl = TMDB_THUMBNAIL_URL + '/' + movie.background;
+  const navigate = useNavigate();
+
+  const handleMovieClick = (id) => {
+    navigate(`/detail/${id}`);
+  };
 
   return (
     <div className='item'>
-      <img className='thumbnail' src={thumbnailFullUrl} alt={title} />
+      <img className='thumbnail' src={thumbnailFullUrl} alt={movie.title} onClick={() => handleMovieClick(movie.id)} />
       <div className='item-desc'>
         <p className='rate'>
           <img src={StarEmptyImage} className='star' />
-          <span>{round(rate, 1)}</span>
+          <span>{round(movie.rate, 1)}</span>
         </p>
-        <strong>{title}</strong>
+        <strong>{movie.title}</strong>
       </div>
     </div>
   );
