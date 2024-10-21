3ffe77fb4e4c50948b6820974aa08fbb72af078a
jinsil <jinsil378900@gmail.com>
2024-10-21 13:51:49 +0900

    refactor: 불필요한 코드 제거

--- src/client/pages/ModalPage.jsx
+++ src/client/pages/ModalPage.jsx
@@ -4,8 +4,6 @@ import MainPage from "./MainPage";
 import MovieModal from "../components/MovieModal";
 
 const ModalPage = ({ movies, movieDetailItem }) => {
-  console.log("modalPage: ", movieDetailItem);
-
   return (
     <>
       <MainPage movies={movies} />
