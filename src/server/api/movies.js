5d104b58f58dd832446c43be69d0f96b1357b138
soosoo22 <lllllh00@naver.com>
2024-10-21 15:53:49 +0900

    feat: ssr을 통한 영화 상세 모달 기능 추가

--- src/server/api/movies.js
+++ src/server/api/movies.js
@@ -1,8 +1,15 @@
-import { FETCH_OPTIONS } from "./endpoints.js";
+import { FETCH_OPTIONS, TMDB_MOVIE_DETAIL_URL } from "./endpoints.js";
 
 export const fetchMovieList = async (url) => {
   const response = await fetch(url, FETCH_OPTIONS);
   const data = await response.json();
 
   return data.results;
+};
+
+export const fetchMovieDetails = async (movieId) => {
+  const response = await fetch(`${TMDB_MOVIE_DETAIL_URL}${movieId}?language=ko-KR`, FETCH_OPTIONS);
+  const data = await response.json();
+
+  return data;
 };
