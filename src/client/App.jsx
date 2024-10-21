95e82505366cd57aafefc3aff5baa81b89434205
soosoo22 <lllllh00@naver.com>
2024-10-21 15:57:01 +0900

    feat: Modal 컴포넌트 구현 및 적용

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 import Header from "./components/Header.jsx";
 import MovieList from "./components/MovieList.jsx";
 import Footer from "./components/Footer.jsx";
+import Modal from "./components/Modal.jsx";
+import { fetchMovieDetails } from "../server/api/movies.js";
 
-const App = ({ nowPlayingMovies }) => {
+const App = ({ nowPlayingMovies, movieDetailItem }) => {
+  const navigate = useNavigate();
+
+  const [movieDetails, setMovieDetails] = useState(movieDetailItem);
+
+  const handleCloseModal = () => {
+    navigate("/");
+
+    setMovieDetails(null);
+  };
+
+  const handleMovieClick = async (movieId) => {
+    const details = await fetchMovieDetails(movieId);
+
+    setMovieDetails(details);
+    navigate(`/detail/${movieId}`);
+  };
+
   return (
     <>
       <Header movie={nowPlayingMovies[0]} />
@@ -13,12 +33,13 @@ const App = ({ nowPlayingMovies }) => {
           <section>
             <h2>지금 인기 있는 영화</h2>
             <ul className="thumbnail-list">
-              <MovieList movies={nowPlayingMovies} />
+              <MovieList movies={nowPlayingMovies} handleMovieClick={handleMovieClick} />
             </ul>
           </section>
         </main>
       </div>
       <Footer />
+      <Modal movieDetailItem={movieDetails} handleCloseModal={handleCloseModal} />
     </>
   );
 };
--- src/client/components/MovieList.jsx
+++ src/client/components/MovieList.jsx
@@ -2,26 +2,25 @@ import React from "react";
 import { TMDB_THUMBNAIL_URL } from "../../server/api/endpoints";
 
 import starEmptyImage from "@images/star_empty.png";
+import { useNavigate } from "react-router-dom";
 
-const MovieList = ({ movies }) => {
+const MovieList = ({ movies, handleMovieClick }) => {
   return movies.map((movie) => (
-    <li>
-      <a href={`/detail/${movie.id}`}>
-        <div className="item">
-          <img
-            className="thumbnail"
-            src={`${TMDB_THUMBNAIL_URL}${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <div className="item-desc">
-            <p className="rate">
-              <img src={starEmptyImage} className="star" />
-              <span>{movie.vote_average}</span>
-            </p>
-            <strong>{movie.title}</strong>
-          </div>
+    <li key={movie.id} onClick={() => handleMovieClick(movie.id)}>
+      <div className="item">
+        <img
+          className="thumbnail"
+          src={`${TMDB_THUMBNAIL_URL}${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div className="item-desc">
+          <p className="rate">
+            <img src={starEmptyImage} className="star" />
+            <span>{movie.vote_average}</span>
+          </p>
+          <strong>{movie.title}</strong>
         </div>
-      </a>
+      </div>
     </li>
   ));
 };
