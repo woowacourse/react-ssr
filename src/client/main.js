6469c852e38166c40519e1c2d95f2db886376e03
hwinkr <dnddl8280@naver.com>
2024-10-21 14:35:14 +0900

    chore: createBrowserRouter를 활용해서 클라이언트 영역 라우팅 처리, hydrateRoot 함수에 전달하는 인자 수정

--- src/client/main.js
+++ src/client/main.js
@@ -2,6 +2,4 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
 
-const { movies } = window.__INITIAL_DATA__;
-
-hydrateRoot(document.getElementById("root"), <App movies={movies} />);
+hydrateRoot(document.getElementById("root"), <App />);
